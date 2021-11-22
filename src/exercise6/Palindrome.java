087c98564d39c11c73578125959689a0664b1568
zlatinas <>
2021-11-22 12:16:35 +0000

    z

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        
+        int reversedNumber = 0;
+        int manipulatedNumber = num;
+        while (manipulatedNumber > 0) {
+          int nextDigit = manipulatedNumber % 10;
+          reversedNumber = reversedNumber * 10 + nextDigit;
+          manipulatedNumber = manipulatedNumber / 10;
+        }
+        boolean palindrome == num == reversedNumber;
+        System.out.println("Palindrome: " + palindrome);
+
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,37 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean passwordValid = false;
+        do {
+          
+          System.out.println("Enter password: ");
+          String pass = input.nextLine();
+
+          if(pass.length() < 8) {
+            continue;
+          }
+          boolean allLettersAndDigits = true;
+          int digitCount = 0;
+          for (int index = 0; index < pass.length(); index++) {
+            char sym = pass.charAt(index);
+            boolean letterOrDigit = Character.isLetterOrDigit(sym);
+            if (!letterOrDigit) {
+              allLettersAndDigits = false;
+              break;
+            }
+
+            boolean digit = Character.isDigit(sym);
+            if (digit) {
+              digitCount++;
+            }
+          }
+          
+          if (allLettersAndDigits && digitCount >= 2) {
+            passwordValid = true;
+          }
+
+        } while (!passwordValid);
+
+        System.out.println("Password valid!");
     }
 }
